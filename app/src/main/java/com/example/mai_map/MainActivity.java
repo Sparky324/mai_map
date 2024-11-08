9ab109b0b047c52d20319b80bdc1bd0e5b5f314e
Sparky324 <elaphantik@mail.ru>
2024-11-08 10:13:07 +0300

    Added intro like a splash_screen.png The picture will be replaced with logo

--- app/src/main/java/com/example/mai_map/MainActivity.java
+++ app/src/main/java/com/example/mai_map/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.mai_map;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.splashScreenTheme);
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Base_Theme_Mai_map);
         setContentView(R.layout.activity_main);
     }
 }
