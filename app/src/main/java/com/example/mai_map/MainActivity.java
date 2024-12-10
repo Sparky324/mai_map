5bfa1c00a2b5f215ac8c8e0bca3073e011461ae8
AnSinV <ansinkinv@gmail.com>
2024-12-11 02:57:03 +0300

    Внесены изменения в функционал смены темы
    - Добавлена смена темы без перезагрузки приложения
    - Удалена SettingsActivity.java за ненадобностью
    - Добавлены кнопки настроек (отвечающая за появление кнопок смены языка и темы), смены языка, смены темы
    - Добавлены иконки для кнопок

--- app/src/main/java/com/example/mai_map/MainActivity.java
+++ app/src/main/java/com/example/mai_map/MainActivity.java
@@ -1,29 +1,36 @@
 package com.example.mai_map;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatDelegate;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     SharedPreferences sharedPreferences;
-
+    boolean showSettings = false;
+    String[] languages = { "RU", "EN", "CN" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.splashScreenTheme);
 
-
         sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
 
         boolean isNightModeOn = sharedPreferences.getBoolean("Theme", false);
+        short langIdx = (short)sharedPreferences.getInt("Lang", 0);
 
         if (isNightModeOn){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
             setTheme(R.style.AppThemeDark);
         } else {
             setTheme(R.style.AppTheme);
@@ -31,21 +38,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button settingsButton = (Button) findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     protected void onStart(){
-
         sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor spEditor = sharedPreferences.edit();
 
+        short langIdx = (short)sharedPreferences.getInt("Lang", 0);
         boolean isNightModeOn = sharedPreferences.getBoolean("Theme", false);
 
         if (isNightModeOn){
@@ -57,12 +56,51 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onStart();
         setContentView(R.layout.activity_main);
 
-        Button settingsButton = (Button) findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(new View.OnClickListener() {
+        ImageButton themeButton = (ImageButton) findViewById(R.id.themeButton);
+        themeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(intent);
+                setTheme(isNightModeOn ? R.style.AppTheme : R.style.AppThemeDark);
+
+                spEditor.putBoolean("Theme", !isNightModeOn);
+                spEditor.apply();
+
+                recreate();
+                overridePendingTransition(0, 0);
+            }
+        });
+
+
+        Button langButton = (Button)findViewById(R.id.langButton);
+        langButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                short langIdx = (short)sharedPreferences.getInt("Lang", 0);
+                langIdx = (short)((langIdx + 1) % languages.length);
+
+                langButton.setText(languages[langIdx]);
+
+                spEditor.putInt("Lang", langIdx);
+                spEditor.apply();
+
+                recreate();
+                overridePendingTransition(0, 0);
+            }
+        });
+
+        ImageButton settingsButton = (ImageButton) findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (showSettings) {
+                    themeButton.setVisibility(GONE);
+                    langButton.setVisibility(GONE);
+                }
+                else {
+                    themeButton.setVisibility(VISIBLE);
+                    langButton.setVisibility(VISIBLE);
+                }
+
+                showSettings = !showSettings;
             }
         });
     }
